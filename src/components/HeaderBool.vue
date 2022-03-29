99433aa3c070ba8b9eb2b52fa34f82d85d438bce
Martino888 <maxmartino33@gmail.com>
2022-03-29 16:21:35 +0200

    header layout

---
+++ src/components/HeaderBool.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <div class="main">
+            <div class="logo text-uppercase  fw-bold fs-4 justify-content-start align-middle">
+                Boollflix
+            </div>
+            <form action="" class="justify-content-end">
+                <input type="text">
+                <button class="btn btn-outline-danger" >Cerca</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderBool'
+}
+</script>
+
+
+<style scoped lang="scss">
+.container{
+    max-width: 100%;
+    background-color: black;
+    height: 60px;
+}
+
+.main{
+    display: flex;
+    justify-content: space-between
+}
+.logo{
+    color: red
+}
+
+input{
+    width: 150px;
+    margin: 10px 50px;
+    border-radius:10px;
+}
+
+</style>
